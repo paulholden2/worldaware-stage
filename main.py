9fc53936463a447188a9a55d99d01e622d42dd9d
Paul Holden <pholden@stria.com>
2019-04-26 09:37:13 -0700

    Update

--- main.py
+++ main.py
@@ -1,16 +1,28 @@
 import sys
 import os
 import comtypes.client
+import extract_msg
 
 wdFormatPdf = 17
 
-in_file = os.path.abspath('./Lorem ipsum.docx')
-out_file = os.path.abspath('./Lorem ipsum.pdf')
+class Stager:
+    def __init__(self):
+        self.word = comtypes.client.CreateObject('Word.Application')
 
-word = comtypes.client.CreateObject('Word.Application')
+    def stage_file(self, source_path, dest_dir, filetype):
+        doc = word.Documents.Open(source_path)
+        doc.SaveAs(os.path.join(dest_dir, os.path.basename(source_path)), FileFormat=wdFormatPdf)
+        doc.Close()
 
-doc = word.Documents.Open(in_file)
-doc.SaveAs(out_file, FileFormat=wdFormatPdf)
-doc.Close();
+    def extract_attachments(self, source_path, dest_dir):
+        msg = extract_msg.Message(source_path)
+        for att in msg.attachments:
+            att.save(useFileName=True, customPath=dest_dir)
 
-word.Quit();
+    def cleanup():
+        self.word.Quit();
+
+##
+
+stager = Stager()
+stager.cleanup()
